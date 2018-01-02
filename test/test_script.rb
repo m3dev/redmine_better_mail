c0e20f104bc8ca90ab0234a5a7caab4bdca17ce1
namutaka <namutaka+g@gmail.com>
2018-01-02 10:39:28 +0900

    add test script

---
+++ test/test_script.rb
@@ -0,0 +1,42 @@
+def test_email
+    user = User.find(1)
+    mail = Mailer.test_email(user).deliver
+end
+
+def issue_add
+    issue = Issue.find(1)
+
+    mail = Mailer.deliver_issue_add(issue)
+end
+
+
+def issue_edit
+    journal = Journal.new(
+      :journalized => Issue.find(1),
+      :user => User.find(1),
+      :created_on => Time.now)
+    journal.details << JournalDetail.new(
+      :property => 'relation',
+      :prop_key => 'relates',
+      :value => 2)
+    Mailer.deliver_issue_edit(journal)
+end
+
+def issue_edit2
+    issue = Issue.find(1)
+    journal = issue.journals.find(20)
+    Mailer.deliver_issue_edit(journal)
+end
+
+Setting.default_language = 'ja'
+#test_email
+issue_add
+issue_edit
+issue_edit2
+
+Setting.default_language = 'en'
+#test_email
+issue_add
+issue_edit
+issue_edit2
+
